d63dccd8856885b78350899199a5098aca781603
EngIbrah <ibrahndagiwe99@gmail.com>
2025-04-13 15:41:45 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        });
+    }
+}
+showMenu('nav-toggle', 'nav-menu');
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text, .process__container, .project__img', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__data, .contact__input', { delay: 400 });
+sr.reveal('.home__social-icon, .work__img, .contact__info', { interval: 200 });
